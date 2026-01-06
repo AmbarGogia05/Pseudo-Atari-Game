375c4031f4030c02985457cf869f6bb54220d50a
Ambar Gogia <cs1240039@cse.iitd.ac.in>
2026-01-07 00:49:31 +0545

    added timer (penalty)
    Comic sans rocks

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ clock = pygame.time.Clock()
 env=Environment()
 renderer = Renderer(env)
 action = np.zeros(2, int)
-flag = np.ones(2, int)
-
+flag = 1
+counter = 0
 while not game_over:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and flag:
+            counter += 1
             if event.key == pygame.K_UP:
                 action = np.array([-1, 0])
             if event.key == pygame.K_DOWN:
@@ -21,12 +22,12 @@ while not game_over:
                 action = np.array([0, -1])
             if event.key == pygame.K_RIGHT:
                 action = np.array([0, 1])
-            if event.key == pygame.K_ESCAPE:
-                game_over = True
-        env.update(flag*action)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            game_over = True
+        env.update(action)
         action = np.array([0, 0])
-    renderer.render_bg()
+    renderer.render_bg(counter)
     if np.array_equal(env.sprite, env.target):
-        flag = np.array([0,0])
+        flag = 0
     clock.tick(60)
 
--- renderer.py
+++ renderer.py
@@ -17,21 +17,26 @@ class Renderer:
         self.screen = pygame.display.set_mode((self.width, self.height))
 
         self.background = pygame.Surface((self.width, self.height))
-        self.render_bg()
 
         self.sprite = env.sprite
 
-    def render_bg(self):
+        pygame.font.init()
+        self.font=pygame.font.SysFont('Comic Sans MS', 30)
+
+    def render_bg(self, counter):
         for y in range(self.env.width):
             for x in range(self.env.height):
                 tile = self.env.bg_color[x, y]
-                if (x != self.env.sprite[0] or y != self.env.sprite[1]) and (x != self.env.target[0] or y != self.env.target[1]):
-                    colors = Tile_color[tile]
-                elif x == self.env.sprite[0] and y == self.env.sprite[1]:
+                colors = Tile_color[tile]
+                if x == self.env.sprite[0] and y == self.env.sprite[1]:
                     colors = (255, 0, 0)
-                else:
+                elif x == self.env.target[0] and y == self.env.target[1]:
                     colors = (0, 0, 255)
+                else:
+                    colors = Tile_color[tile]
                 rect = pygame.Rect(y*self.cell, x*self.cell, self.cell, self.cell)
                 pygame.draw.rect(self.background, colors, rect)
+        text_surface = self.font.render(str(counter), True, (255, 255, 255))
         self.screen.blit(self.background, (0, 0))
+        self.screen.blit(text_surface, (590, 10))
         pygame.display.flip()

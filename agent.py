c637963011bc70f3dd3f62ecc840aa2a8ac60bef
Preesha Agrawal <cs5240888@cse.iitd.ac.in>
2026-01-18 10:22:54 +0530

    Modify Q-learning agent to adjust for variable game environment
    Attempt to handle moving target, develop different policy for each case of starting position being bottom left, center and right.

--- agent.py
+++ agent.py
@@ -6,7 +6,8 @@ import time
 class Agent:
     def __init__(self, epsilon=0.2):
         self.state = np.array([0, 0])
-        self.Q = np.random.uniform(low=0, high=10.0, size=(4, 10, 10)) #Q(s,a)
+        self.Q = np.random.uniform(low=0, high=10.0, size=(3, 4, 10, 10)) #Q(s,a)
+        np.save('q_table_data.npy', self.Q)
         self.policy = np.zeros((10, 10), int)
         self.greedy = np.zeros((10, 10), int)
         self.epsilon = epsilon
@@ -20,12 +21,17 @@ class Agent:
 
     def QL(self, episodes=100):
         arr = np.array([0,1,2,3])
+        i=3
         for episode in range(episodes):
             self.game = Game()
+            if(self.game.env.sprite[1]==9):
+                i=2
+            else :
+                i=self.game.env.sprite[1]//3
             while self.game.counter < 150 and not self.game.game_over:
                 self.get_state()
                 old_state = self.state
-                self.greedy = np.argmax(self.Q, axis=0)
+                self.greedy = np.argmax(self.Q[i], axis=0)
                 num=np.random.uniform(0,1)
                 if num<self.epsilon:
                     self.policy[self.state[0]][self.state[1]] = np.random.randint(0,3)
@@ -48,9 +54,11 @@ class Agent:
                 self.game.step()
                 self.get_state()
                 a=self.policy[old_state[0]][old_state[1]]
-                Q_sa = self.Q[a][old_state[0]][old_state[1]]
-                Q_next = np.max(self.Q[:,self.state[0],self.state[1]], axis = 0)
-                self.Q[a][old_state[0]][old_state[1]] = Q_sa + self.alpha * (self.game.reward + self.gamma * Q_next - Q_sa)
-                time.sleep(0.1)
+                Q_sa = self.Q[i][a][old_state[0]][old_state[1]]
+                Q_next = np.max(self.Q[i][:,self.state[0],self.state[1]], axis = 0)#
+                self.Q[i][a][old_state[0]][old_state[1]] = Q_sa + self.alpha * (self.game.reward + self.gamma * Q_next - Q_sa)
+                time.sleep(0.001)
 agent = Agent(0.2)
-agent.QL(20)
+
+agent.QL(60)
+np.save('q_table_data_learnt.npy', agent.Q)
